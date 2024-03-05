ecf2ec1644a381a8e02ed733ecb258cfbbe08dd7
largek2 <largek2@uni.coventry.ac.uk>
2024-03-05 10:45:40 +0000

    Initial commit

---
+++ 6003CEM/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _6003CEM;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ 6003CEM/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace _6003CEM;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
